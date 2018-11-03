09caa3a623cc7ea8eba9bbebe23c008e10c259ce
Noah McConnell <nonomcconnell2@gmail.com>
2018-11-03 17:15:31 -0600

    fully fuctioning form

--- email.js
+++ email.js
@@ -1,21 +1,49 @@
-function contactNoah(){
-  var data = {
-    service_id: 'sendgrid',
-    template_id: 'template_ZeNYFH5o',
-    user_id: 'user_pF7I4OkKTtl1xFdPwZg3b',
-    template_params: {
-      'from_name':$('#name').val(),
-      'from_phone':$('#phone').val(),
-      'from_message':$('#message').val(),
-      'from_email':$('#email').val(),   
-    }
+function contactNoah() {
+  var valid = $("#validMessage");
+  if ($("#name").val() === "") {
+    valid.text("Name Required");
+    valid.show();
+    return;
+  }
+  if ($("#phone").val() === "") {
+    valid.text("Phone Required");
+    valid.show();
+    return;
+  }
+  if ($("#email").val() === "") {
+    valid.text("Email Required");
+    valid.show();
+    return;
+  }
+  if ($("#message").val() === "") {
+    valid.text("Message Required");
+    valid.show();
+    return;
   }
 
-  axios.post('https://api.emailjs.com/api/v1.0/email/send', data)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  valid.text("")
+  valid.hide()
+
+
+  $("#sendMessageButton").prop("disabled", true);
+  var data = {
+    service_id: "sendgrid",
+    template_id: "template_ZeNYFH5o",
+    user_id: "user_pF7I4OkKTtl1xFdPwZg3b",
+    template_params: {
+      from_name: $("#name").val(),
+      from_phone: $("#phone").val(),
+      from_message: $("#message").val(),
+      from_email: $("#email").val()
+    }
+  };
+
+  axios
+    .post("https://api.emailjs.com/api/v1.0/email/send", data)
+    .then(function(response) {
+      $("#successMessage").show();
+    })
+    .catch(function(error) {
+      $("#errorMessage").show();
+    });
 }
